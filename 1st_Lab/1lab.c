0206eec8f4c43436bbed015e27bd280696630a8a
Nikita Yakovlev <54926478+SWITED@users.noreply.github.com>
2020-09-08 14:39:15 +0700

    fixed

--- 1st_Lab/1lab.c
+++ 1st_Lab/1lab.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define PTHREAD_CREATE_SUCCESS 0
 #define LINES_COUNT 10
 
-char* stringForParentThread = "Parent string\n";
-char* stringForChildThread = "Child string\n";
-
-void print_lines(char* line, int count) {
-        int i = 0;
-        for(int i = 0; i < count; i++) {
-                printf("%s", line);
+void print_ten_strings(const char* name){
+        for (int i = 0; i < LINES_COUNT; i++){
+                printf("%s : String %d\n", name, i);
         }
 }
 
-void* thread_control(void* arg) {
-        print_lines(stringForChildThread, LINES_COUNT);
-        return NULL;
+
+void* thread_func(void* args){
+        print_ten_strings((char*)args);
+        pthread_exit(NULL);
 }
 
-int main() {
-        int result;
-        pthread_t child;
-        result = pthread_create(&child, NULL, thread_control, NULL);
-        if(result != PTHREAD_CREATE_SUCCESS) {
-                perror("Error while creating a thread");
+
+int main(){
+        pthread_t thread;
+        int status;
+        static char sec_th[] = "Second Thread";
+        status = pthread_create(&thread, NULL, thread_func, (void*)sec_th);
+        if (status != PTHREAD_CREATE_SUCCESS){
+                perror("Error in creating thread\n");
                 return EXIT_FAILURE;
         }
-        print_lines(stringForParentThread, LINES_COUNT);
+        print_ten_strings("Main thread");
         pthread_exit(NULL);
 }
 
